baf6ebadd718747d186efcb160da3fa5be8f7c43
Joao Anselmo <joao.anselmo2@gmail.com>
2013-06-26 20:48:06 +0200

    Rspec: Add spec

--- spec/htmldiff_spec.rb
+++ spec/htmldiff_spec.rb
@@ -81,7 +81,35 @@ describe "htmldiff" do
     |
     doc_b = %|<p>Nothing!</p>|
     diff = TestDiff.diff(doc_a, doc_b)
-    puts diff
+    diff.should == %|<p><del class="diffmod"> Test Paragraph </del><ins class="diffmod">Nothing!</ins></p><del class="diffdel">
+    </del><del class="diffdel"><p><del class="diffdel"> </del></p><del class="diffdel">
+    </del><table><tbody><tr><td><del class="diffdel">hello</del></td><td><del class="diffdel">bye</del></td></tr></tbody></table><del class="diffdel">
+    </del><p><del class="diffdel">&nbsp;</del></p><del class="diffdel">
+    </del></del>|
+  end
+
+  it "should wrap deleted table rows" do
+    doc_a = %|<p>my table</p>
+    <table>
+    <tbody>
+    <tr><td>hello</td><td>bye</td></tr>
+    <tr><td>remove</td><td>me</td></tr>
+    </tbody>
+    </table>|
+    doc_b = %|<p>my table</p>
+    <table>
+    <tbody>
+    <tr><td>hello</td><td>bye</td></tr>
+    </tbody>
+    </table>|
+    diff = TestDiff.diff(doc_a, doc_b)
+    diff.should == %|<p>my table</p>
+    <table>
+    <tbody>
+    <tr><td>hello</td><td>bye</td></tr>
+    <del class="diffdel"><tr><td><del class="diffdel">remove</del></td><td><del class="diffdel">me</del></td></tr><del class="diffdel">
+    </del></del></tbody>
+    </table>|
   end
 
 end
