10966c55b5beb466ba23b87c5c0041668dc0c01a
Matt Gibson <downrightlies@gmail.com>
2015-07-21 23:16:48 +0100

    Move classes to their own files and tidy up code style

---
+++ lib/htmldiff/match.rb
@@ -0,0 +1,11 @@
+module HTMLDiff
+  Match = Struct.new(:start_in_old, :start_in_new, :size) do
+    def end_in_old
+      self.start_in_old + self.size
+    end
+
+    def end_in_new
+      self.start_in_new + self.size
+    end
+  end
+end
